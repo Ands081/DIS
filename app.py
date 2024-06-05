bc179617b2a8ea23710cef7615c41c1533a93ba6
Peter-BP <peterbock@live.dk>
2024-06-05 17:40:34 +0200

    s

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-db = "dbname='kebab' user='postgres' host='127.0.0.1' password = '123'"
+db = "dbname='kebab' user='postgres' host='localhost' password = '123'"
 conn = psycopg2.connect(db)
+
+@app.route("/")
+def home():
+    return 'Velkommen til min Flask-app!'
 
 if __name__ == '__main__':
     app.run(debug=True)
