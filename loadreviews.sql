4a508e4231c051a2db71da270ee0674884bd6372
Peter-BP <peterbock@live.dk>
2024-06-08 15:08:37 +0200

    db done

--- loadreviews.sql
+++ loadreviews.sql
@@ -4,7 +4,7 @@ rating float(2),
 review text);
 
 copy  reviews(id, rating, review)
-            from 'C:\Users\Peter\Desktop\Durum\src\database\Reviews.csv'
+            from 'C:\Users\Peter\Desktop\Durum\src\database\reviews.csv'
             delimiter ','
             CSV HEADER
             ENCODING 'UTF8';
